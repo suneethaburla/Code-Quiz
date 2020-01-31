a7af710814f7f0b6e0919317d45e1b31ba6144c3
suneethaburla <suneetha@gmail.com>
2020-01-31 13:15:05 -0800

    Added start timer function, eventListener for startgame button, showDuestionDiv function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,41 +1,60 @@
-console.log(questions);
-//On load function
-let gameStartEl = document.getElementById("welcome");
+
+
+//create all the elements in the gameStartDiv
+let gameStartDivEl = document.getElementById("gameStartDiv");
+let welcomeEl = document.getElementById("welcome");
 let instructionsEl = document.getElementById("instructions");
 let startButtonEl = document.getElementById("startButton");
+let questionsDivEl = document.getElementById("questionsDiv");
 
-window.addEventListener("load", function(event) {
-    gameStartEl.textContent = "Welcome to Code Quiz!";
+
+//On load function
+window.addEventListener("load", function (event) {
+    //textContent for all elements in the gameStartDiv
+    questionsDiv.style.display = "none";
+    welcomeEl.textContent = "Welcome to Code Quiz!";
     instructionsEl.textContent = "Try to answer the following code related questions in the time limit. Keep in mind that the incorrect answers will penalize your time by 5 seconds";
     startButtonEl.textContent = "Start Quiz!";
+
 });
 
-//Timer function
+//Start Timer function
 let timeEl = document.getElementById("time");
 let mainEl = document.getElementById("main");
 
-var secondsLeft = 90;
+var secondsLeft = 180;
+startButtonEl.addEventListener("click", function (event) {
+    gameStartDiv.style.display = "none";
+    questionsDiv.style.display = "block";
+    setTime();
+    showQuestion();
+});
 
 function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft;
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      endGame();
-    }
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            endGame();
+        }
 
-  }, 1000);
+    }, 1000);
 }
 function endGame() {
-    timeEl.textContent = " ";
-  
+    timeEl.textContent = "0";
+
     var imgEl = document.createElement("img");
-  
+
     imgEl.setAttribute("src", "images/image_1.jpg");
     mainEl.appendChild(imgEl);
-  
-  }
-  
-  setTime();
+
+}
+
+function showQuestion() {
+    for(let i=0;i<questions.length;i++) {
+        console.log(questions[i]);
+    }
+}
+
