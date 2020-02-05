240d0aa58d171fe06537ac5258219f090ce45bc3
suneethaburla <suneetha@gmail.com>
2020-02-05 11:13:21 -0800

    Updated app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,65 @@
 
-
-//create all the elements in the gameStartDiv
+//create all the elements
+let viewHighScoreEl = document.getElementById("viewHighScore");
 let gameStartDivEl = document.getElementById("gameStartDiv");
 let welcomeEl = document.getElementById("welcome");
 let instructionsEl = document.getElementById("instructions");
 let startButtonEl = document.getElementById("startButton");
+let nextButtonEl = document.getElementById("nextButton");
+let timeEl = document.getElementById("time");
 let questionsDivEl = document.getElementById("questionsDiv");
-let titleDivEl = document.getElementById("titleDiv");
+let questionDivEl = document.getElementById("questionDiv");
+let questionEl = document.getElementById("question");
 let answerChoicesEl = document.getElementById("answerChoices");
+let userChoice = document.querySelector('input[type=radio]');
+let choiceEl = document.getElementsByClassName("choice");
+let choiceEl1 = document.getElementById("choice1");
+let choiceEl2 = document.getElementById("choice2");
+let choiceEl3 = document.getElementById("choice3");
+let choiceEl4 = document.getElementById("choice4");
+let resultsDivEl = document.getElementById("resultsDiv");
+let msgEl = document.getElementById("msg");
+let highScoresDivEl = document.getElementById("highScoresDiv");
+let highScoresBtnEl = document.getElementById("highScoresBtn");
 
 
+// Initializing the variables
+let score = 0;
+let secondsLeft = 90;
+let highScore = 0;
+let questionIndex;
+let totQuestions = questions.length;
 
-//On load function
-window.addEventListener("load", function (event) {
-    //textContent for all elements in the gameStartDiv
-    questionsDiv.style.display = "none";
-    welcomeEl.textContent = "Welcome to Code Quiz!";
-    instructionsEl.textContent = "Try to answer the following code related questions in the time limit. Keep in mind that the incorrect answers will penalize your time by 5 seconds";
-    startButtonEl.textContent = "Start Quiz!";
+
+//On load function, hide all the divs and show gameStart Div
+window.addEventListener("load", function () {
+    questionsDivEl.style.display = "none";
+    highScoresDivEl.style.display = "none";
+    resultsDivEl.style.display = "none";
+    gameStartDivEl.style.display = "block";
 
 });
 
-//Start Timer function
-let timeEl = document.getElementById("time");
-let mainEl = document.getElementById("main");
-
-var secondsLeft = 180;
+// Click eventListener to start Quiz
 startButtonEl.addEventListener("click", function (event) {
     gameStartDiv.style.display = "none";
+    resultsDivEl.style.display = "none";
     questionsDiv.style.display = "block";
-    setTime();
+    highScoresDivEl.style.display = "none";
+    questionIndex = 0;
+    startTimer();
     setNextQuestion();
 });
 
-function setTime() {
+highScoresBtnEl.addEventListener("click", function (event) {
+    displayHighScore()
+})
+viewHighScoreEl.addEventListener("click", function (event) {
+    displayHighScore()
+})
+
+//Start Timer function
+function startTimer() {
     var timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft;
@@ -48,52 +74,91 @@ function setTime() {
 function endGame() {
     timeEl.textContent = "0";
 
-    var imgEl = document.createElement("img");
+}
 
-    imgEl.setAttribute("src", "images/image_1.jpg");
-    mainEl.appendChild(imgEl);
+function decrementTimer() {
+    secondsLeft=secondsLeft-5;
 
 }
 
-function setNextQuestion() {
-    for (let i = 0; i < questions.length; i++) {
-        titleDivEl.textContent = (i + 1) + '. ' + questions[i].title;
-        questions[i].choices.forEach(choice => {
-            const choiceEl = document.createElement("li");
-            choiceEl.textContent = choice;
-            titleDivEl.appendChild(choiceEl);
-            const resultEl = document.createElement("p");
+function displayHighScore() {
+let initials = localStorage.getItem("initials");
+let highScore = localStorage.getItem("highScore");
+displayHighScore.textContent = initials + ":" + highScore;
+}
 
-        })
-        choiceEl.addEventListener("click", function (event) {
-            if (event.target.matches("li")) {
-                if (choices[event.target.parentElement.id] === questions[i].answer) {
+function displayMessage(type, message) {
+    msgDiv.textContent = message;
+    msgDiv.setAttribute("class", type);
+  }
+
+
+function setNextQuestion() {
+    showQuestion(questions[questionIndex])   
+}
+
+// Click event listener to nextButton
+nextButtonEl.addEventListener("click", function (event) {
+    showQuestion(questions[questionIndex]) 
+})
+
+// Click event listener to check answer
+userChoice.addEventListener("click", function (event) {
+    if(!userChoice) {
+        alert("please select an answer");
+        return;
+    }
+    let userAnswer = userChoice.nodeValue;
+    if(questions[questionIndex].answer === userAnswer) {
+        console.log("correct");
+        displayMessage("success", "Registered successfully");
+        score++;
+    }
+    else {
+        console.log("wrong");
+        displayMessage("error", "Email cannot be blank");
+        decrementTimer()
+    }
+    // userChoice.checked = false;
+    questionIndex++;
+    console.log(questionIndex);
+    if(questionIndex === totQuestions -1) {
+        nextButtonEl.textContent = "Finish";
+    }
+    if(questionIndex === totQuestions) {
+        questionsDiv.style.display = "none";
+        gameStartDiv.style.display = "none";
+        resultsDiv.style.display = "block";
+        highScoresDivEl.style.display = "none";
+        localStorage.setItem("initials", initials);
+        localStorage.setItem("highScore", highScore);
+        renderLastRegistered();
+
+    }
+})
+
+
+function showQuestion(question) {
+    // need to write the below cleaner
+    questionEl.textContent = question.question;
+    choiceEl1.textContent =question.choices[0];
+    choiceEl2.textContent =question.choices[1];
+    choiceEl3.textContent =question.choices[2];
+    choiceEl4.textContent =question.choices[3];
+    
+    }
+
+function checkAnswer() {
+
+            if (event.target.matches("button")) {
+                console.log("hello");
+                if (choices[event.target.id] === questions[i].answer) {
                     resultEl.textContent = "correct!"
+                    score += score;
                 }
                 else {
                     resultEl.textContent = "wrong!"
                 }
             }
-        })
-    }
+
 }
-
-
-// function checkAnswer() {
-//     choiceEl.addEventListener("click", function(event) {
-//         if(event.target.matches("li")) {
-//             if (choices[event.target.parentElement.id] === questions[i].answer){
-//                 resultEl.textContent = "correct!"
-//             }
-//             else {
-//                 resultEl.textContent = "wrong!"
-//             }
-//           }
-//     })
-// }
-
-
-// var div = document.createElement('div');
-// div.textContent = "Sup, y'all?";
-// div.setAttribute('class', 'note');
-// document.body.appendChild(div);
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,6 +1,6 @@
 const questions = [
     {
-      title: "Which of the following is NOT a javascript data type?",
+      question: "Which of the following is NOT a javascript data type?",
       choices: ["Array", "Object", "Table", "String"],
       answer: "Table"
     },
@@ -13,5 +13,21 @@ const questions = [
       title: "What will this statement return: console.log('7' == 7)?",
       choices: ["true", "false", "maybe", "depends"],
       answer: "true"
+    },
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: ["<script>", "<scripting>", "<javascript>", "<js>"],
+      answer: "<script>"
+    },
+    {
+      title: "Where is the correct place to insert a JavaScript?",
+      choices: ["The <body> section", "The <head> section", "<Both the <body> section and <head> section", "Neither of the above"],
+      answer: "The <body> section"
+    },
+    {
+      title: `How do you write "Hello World" in an alert box?`,
+      choices: [`alert("Hello World")`, `msgBox("Hello World")`, `alertBox("Hello World")`, `msg("Hello World")`],
+      answer: `alert("Hello World")`
     }
+
   ];
